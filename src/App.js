71ae3a6b334601eec695775edcebb3acdae59d65
Justin Rosecrans <justin.rosecrans@tdameritrade.com>
2019-09-04 12:54:24 -0500

    Cards Class

--- src/App.js
+++ src/App.js
@@ -1,38 +1,33 @@
-import React from 'react';
+// async function getCards() {
+//   let getCards = {};
 
-async function getCards() {
-  let getCards = {};
-
-  try {
-    getCards = await fetch('https://deckofcardsapi.com/api/deck/g1swml2iwvv0/draw/?count=5');
-  } catch (error) {
-    return Promise.reject(error);
-  }
+//   try {
+//     getCards = await fetch('https://deckofcardsapi.com/api/deck/g1swml2iwvv0/draw/?count=5');
+//   } catch (error) {
+//     return Promise.reject(error);
+//   }
   
-  const json = await getCards.json();
-  const value = json.cards;
+//   const json = await getCards.json();
+//   const value = json.cards;
 
-  value.forEach(function(element, i) {
-    document.querySelector('.hand').insertAdjacentHTML('afterbegin', `<img src=${element.image} />`);
-  });
-}
+//   value.forEach(function(element, i) {
+//     document.querySelector('.hand').insertAdjacentHTML('afterbegin', `<img src=${element.image} />`);
+//   });
+// }
 
-const score = {
-  "Three of a kind" : []
-}
+import React from 'react';
+import Cards from './cards/Cards';
 
-function displayJoke() {
+function showHand() {
   const element = (
     <section>
       <h1>5 card stud poker</h1>
       <span className='hand'></span>
-      {/* <p>Your Hand is: {score}</p> */}
+      <Cards />
     </section>
   );
-
-  getCards();
 
   return element;
 }
 
-export default displayJoke;
+export default showHand;
--- src/cards/cards.js
+++ src/cards/cards.js
@@ -1,27 +1,46 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from '../../node_modules/react';
 
 class Cards extends Component {
-    constructor() {
-        super();
-        this.state = {
-            cards: []
-        };
-    }
+  constructor() {
+    super();
+    this.state = {
+      cards: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://deckofcardsapi.com/api/deck/new/draw/?count=5')
+      .then((response) => {
+        return response.json();
+      })
+      .then((myJson) => {
+        let cardsArr = [];
+
+        myJson.cards.forEach((card) => {
+          cardsArr.push(card);
+        });
+
+        this.setState({
+          cards: cardsArr
+        })
+      });
+  }
+
+  render() {
+    const cards = this.state.cards;
+
+    return (
+      <p>
+        {cards.map(card =>
+          <span>
+            <img alt="card" src={card.image} />
+          </span>
+        )}
+      </p>
+    );
+  }
 }
 
-async function getCards() {
-    let getCards = {};
-  
-    try {
-      getCards = await fetch('https://deckofcardsapi.com/api/deck/g1swml2iwvv0/draw/?count=5');
-    } catch (error) {
-      return Promise.reject(error);
-    }
-    
-    const json = await getCards.json();
-    const value = json.cards;
-  
-    value.forEach(function(element, i) {
-      document.querySelector('.hand').insertAdjacentHTML('afterbegin', `<img src=${element.image} />`);
-    });
-  }
+export default Cards;
